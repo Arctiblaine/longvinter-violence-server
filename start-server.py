6fd9c25cf108267fcbd92352ed3b466b91f82176
Arctiblaine <icepolice@email.arizona.edu>
2022-03-07 19:53:49 -0700

    added a python runner

---
+++ start-server.py
@@ -0,0 +1,9 @@
+### Arctiblaine - 3/7/2022
+### start-server.py - a thinly veiled attempt at running exe files
+###     through python files.
+import subprocess
+def main():
+    subprocess.call("LongvinterServer.exe")
+
+if __name__ == "__main__":
+    main()
